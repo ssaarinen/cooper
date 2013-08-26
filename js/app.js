a083fe15a64b72aeb19ffb72029f09a7ba9a8173
Samuli Saarinen <samuli.saarinen@gmail.com>
2013-08-26 18:41:17 +0300

    restructured js and changed bootstrap to foundation

--- js/app.js
+++ js/app.js
@@ -2,61 +2,111 @@
  * Created by samuli on 20.8.2013.
  */
 
-function CooperCtrl($scope, $timeout) {
-    $scope.btnText = "Start";
-    $scope.startDate = "not set";
+var app = angular.module("cooperApp", []);
 
-    $scope.eventLengthMin = 12;
-    $scope.lapLength = 400
-    $scope.laps = 0;
-    $scope.endDistance = 0;
-    $scope.timeLeft = 0;
+app.controller("CooperCtrl", function($scope, $timeout) {
+
+    $scope.cooper = new Cooper({lapLength:400, eventLengthMin:12});
 
     var updateTimer;
 
     $scope.start = function () {
-        if ($scope.startTime) {
-            $scope.lapDone();
+        if ($scope.cooper.started) {
+            $scope.cooper.lapDone();
         } else {
-            $scope.startTime = new Date().getTime();
-            $scope.timeLeft = new Date($scope.eventLengthMin*60000);
-            $scope.endTime = new Date($scope.startTime + $scope.eventLengthMin * 60000).getTime();
-            $scope.startDate = new Date($scope.startTime);
-            $scope.btnText = "Lap";
-            $scope.laps = 0;
+            $scope.cooper.start();
             updateTimer =  $timeout($scope.updateTime, 1000);
 
         }
     }
 
-    $scope.lapDone = function () {
-        var timeUsed = new Date().getTime() - $scope.startTime;
-        var timeLeft = $scope.endTime - new Date().getTime();
-        $scope.timeLeft = new Date(timeLeft);
-
-        if(timeLeft > 0) {
-            $scope.laps+=1;
-            var distance = $scope.laps * $scope.lapLength;
-            var speed = distance / (timeUsed / 1000); //m/s
-            $scope.endDistance = distance + (speed * (timeLeft / 1000));
-        } else {
-            $timeout.cancel(updateTimer);
-            $scope.btnText = "Start";
-            $scope.startTime = null;
-            $scope.timeLeft = 0;
-        }
-
-    }
-
     $scope.updateTime = function() {
-        var timeUsed = new Date().getTime() - $scope.startTime;
-        var timeLeft = $scope.endTime - new Date().getTime();
-        if(timeLeft > 0) {
-            $scope.timeLeft = new Date(timeLeft);
-            $timeout($scope.updateTime, 1000);
-        } else {
-            $scope.timeLeft = new Date(0);
+        $scope.cooper.updateTime();
+        if($scope.cooper.started) {
+            updateTimer = $timeout($scope.updateTime, 1000);
         }
     }
+} );
 
+app.directive("cooperSettings", function() {
+    return {
+        restrict:"E",
+        templateUrl:"tpl/cooper-settings.html"
+    };
+});
+
+app.directive("cooperCounter", function() {
+    return {
+        restrict:"E",
+        templateUrl:"tpl/cooper-counter.html"
+    };
+});
+
+var INTEGER_REGEXP = /^\-?\d*$/;
+app.directive('integer', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function(viewValue) {
+                if (INTEGER_REGEXP.test(viewValue)) {
+// it is valid
+                    ctrl.$setValidity('integer', true);
+                    return viewValue;
+                } else {
+// it is invalid, return undefined (no model update)
+                    ctrl.$setValidity('integer', false);
+                    return undefined;
+                }
+            });
+        }
+    };
+});
+
+function Cooper(options) {
+    options = options || {};
+    this.eventLengthMin = options.eventLengthMin || 12;
+    this.lapLength = options.lapLength || 400;
+    this.reset();
 }
+
+Cooper.prototype.reset = function() {
+    this.started = false;
+    this.endDistance = 0;
+    this.timeLeft = this.eventLengthMin * 60000;
+    this.laps = 0;
+    this.timeLeftDate = new Date(this.timeLeft);
+}
+
+Cooper.prototype.start = function() {
+    this.reset();
+    this.startTime = new Date().getTime();
+    this.endTime = new Date(this.startTime + this.timeLeft).getTime();
+    this.started = true;
+};
+
+Cooper.prototype.stop = function() {
+    this.started=false;
+};
+
+Cooper.prototype.lapDone = function() {
+    this.updateTime();
+
+    if(this.timeLeft > 0) {
+        this.laps+=1;
+        var distance = this.laps * this.lapLength;
+        var speed = distance / (this.timeUsed / 1000); //m/s
+        this.endDistance = distance + (speed * (this.timeLeft / 1000));
+    }
+};
+
+Cooper.prototype.updateTime = function() {
+    if(this.started) {
+        var now = new Date().getTime();
+        this.timeUsed = now - this.startTime;
+        this.timeLeft = this.endTime - now;
+        this.timeLeftDate = new Date(this.timeLeft);
+        if(this.timeLeft <= 0) {
+            this.stop();
+        }
+    }
+};
